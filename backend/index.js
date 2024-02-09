6476d6983c917452238421c4e54ffb931977875a
rahulkale22 <kale42701@gmail.com>
2024-02-09 19:03:58 +0530

    routes minor changes

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,10 @@ const mainRouter = require("./routes/index");
 const app = express();
 
 app.use("/api/v1", mainRouter);
+
+//     /api/v1/user/signup
+//     /api/v1/user/signin
+//     /api/v1/user/changePassword
+
+//      /api/v1/account/transferMoney
+//      /api/v1/account/balance
