995d5f040d307fd4819326b05a1eba14f71fe79c
rahulkale22 <kale42701@gmail.com>
2024-02-09 17:29:42 +0530

    minor fix

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://kale42701:lfOmf97q2wIPe8M1@cluster0.mgggyf6.mongodb.net/paytm");
+const userSchema = mongoose.Schema({
+    username : String,
+    password : String,
+    firstName : String,
+    lastName : String
+});
+
+const User = mongoose.model("User",userSchema);
+
+
+module.exports = {
+    User
+}
